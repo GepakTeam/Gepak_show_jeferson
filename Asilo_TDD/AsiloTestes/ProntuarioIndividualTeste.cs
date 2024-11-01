51bd496bf5b505ca243add0d402936af526a904e
Pedro Pivetta <teamgepak@gmail.com>
2024-11-01 15:38:15 -0300

    Update

---
+++ Asilo_TDD/AsiloTestes/ProntuarioIndividualTeste.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsiloTestes
+{
+    
+    public class ProntuarioIndividualTeste
+    {
+        [Fact]
+        public void Check_Gender_Masculino_Ok() 
+        {
+
+
+
+            //Arrange (Instancia)
+
+            //var prontIndividual = new ProntuarioIndividual();
+            bool gender = false;
+
+
+            //Act (Valor)
+
+
+
+            //Asset (Resultado)
+            Assert.False(gender);
+
+        }
+    }
+}
