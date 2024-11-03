bf267703adf5bffb7fb9a0a2d0e91e8fe92a7eb2
Pedro Pivetta <teamgepak@gmail.com>
2024-11-03 18:27:45 -0300

    Finish

--- Asilo_TDD/AsiloTestes/obj/Debug/net8.0/AsiloTestes.AssemblyInfo.cs
+++ Asilo_TDD/AsiloTestes/obj/Debug/net8.0/AsiloTestes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AsiloTestes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51bd496bf5b505ca243add0d402936af526a904e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AsiloTestes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AsiloTestes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
